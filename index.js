635df2c969c02bde30c6584695f1f462b9b01140
Anukriti Bagchi <91126891+anukriti-bagchi@users.noreply.github.com>
2021-09-22 13:26:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//functions
+//processing
+function mulitply(numberOne, numberTwo){
+  console.log("numberOne:",numberTwo, "numberTwo:", numberTwo)
+ var prod = numberOne * numberTwo;
+ return prod;
+}
+
+//use the function
+var result = mulitply (2,5);
+console.log("The product of 2 and 5 is: " + result);
+
+
+function add(numberOne, numberTwo){
+  console.log("numberOne:",numberTwo, "numberTwo:",numberTwo)
+ var sum = numberOne + numberTwo;
+ return sum;
+
+}
+
+var result2 = add (2,5);
+console.log("The sum of 2 and 5 is: " + result2);
+ 
+ 
